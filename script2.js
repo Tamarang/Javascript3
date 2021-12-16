fdbc04311ff96a886a9ee2ff58a0d4899a4b0bc5
Jan Siepel <91777292+Tamarang@users.noreply.github.com>
2021-12-16 19:12:24 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+const age = 51;
+const isFemale = false;
+const driverStatus = 'bob';
+
+if(age >= 18)
+{
+    console.log("you're allowed in");
+}
+else
+{
+    console.log("you're too young and is kindly requested to stay outside.");
+}
+
+if (isFemale == true) {
+    console.log("You're allowed to buy tickets");
+}
+else
+{
+    console.log("You're not allowed to buy tickets since its ladies night");
+}
+
+if (driverStatus == 'bob') {
+    console.log("You're allowed to drive");
+}
+else
+{
+    console.log("You do not have the bob status and is therefore not allowed to drive.");
+}
